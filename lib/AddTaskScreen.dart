c0c89d9c1522095ff5f55d2ca024849e39416720
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-04 12:39:51 +0530

    All UI WORK Done

--- lib/AddTaskScreen.dart
+++ lib/AddTaskScreen.dart
@@ -5,18 +5,43 @@ class AddTaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-            topLeft:Radius.circular(25),
-            topRight: Radius.circular(25)
+    return SingleChildScrollView(
+      child: Container(
+       // padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        color: Color(0xff757575),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft:Radius.circular(25),
+                topRight: Radius.circular(25)
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("Add Task",style: TextStyle(
+                fontSize:30,
+
+                color: Colors.lightBlueAccent,
+                fontWeight: FontWeight.bold,
+              ),textAlign: TextAlign.center,),
+              SizedBox(height: 10,),
+              TextField(
+                cursorColor: Colors.blueAccent,
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10,),
+              FlatButton(
+                  onPressed: (){},
+                  child:Text("Add"),
+                color: Colors.lightBlueAccent,
+              )
+            ],
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          TextField()
-        ],
       ),
     );
   }
